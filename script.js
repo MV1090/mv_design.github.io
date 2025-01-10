99dd737ab972fa52a4c00d4216156da4358bd9c3
Mark <mark.vale@torontofilmschool.ca>
2025-01-10 13:57:39 -0700

    Boiler plate code for 3 pages with working buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+// This is where you can add any interactivity, if needed
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Website Loaded");
+
+    // Example: A simple greeting in the console
+    const pageTitle = document.title;
+    console.log("You are currently viewing the " + pageTitle + " page.");
+});
